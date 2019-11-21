7478c5555de424f9b6d32383d40c57aeed4876b3
Ofoty <53044034+Ofoty@users.noreply.github.com>
2019-11-21 17:42:35 +0500

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,32 @@ module.exports = {
     filename: '[name].[contenthash].js',
     chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    pathinfo: false,
     publicPath: '/',
   },
+  module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader',
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            'file-loader',
+          ],
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use: [
+            'file-loader',
+          ],
+        },
+      ],
+    },
   optimization: {
     runtimeChunk: 'single',
     moduleIds: 'hashed',
